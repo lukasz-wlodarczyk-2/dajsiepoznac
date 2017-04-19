2c85c81944371565412f6c17c4b2161272494530
Łukasz Włódarczyk <lukasz.wlodarczyk@docflow.pl>
2017-04-19 14:03:14 +0200

    added spring seciurity and actuator

---
+++ src/main/java/com/dajsiepoznac/configuration/MvcConfig.java
@@ -0,0 +1,24 @@
+package com.dajsiepoznac.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/dashboard").setViewName("dashboard");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/");
+    }
+}
